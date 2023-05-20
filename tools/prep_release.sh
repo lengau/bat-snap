959543548f742628cf2b0280e507826915e038b3
Alex Lowe <lengau@gmail.com>
2023-05-20 13:39:32 -0400

    Initial snap

---
+++ tools/prep_release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Prepare the repository for the changes coming from an upstream release.
+
+set -eu -o pipefail
+
+upstream_release=$1
+release_tag=$2
+release_file=$3
+
+echo "Preparing upstream release ${upstream_release}"
+
+echo "Setting snap version to ${upstream_release}"
+sed -i 's/^\(version: \).*$/\1'"${upstream_release}"'/' snap/snapcraft.yaml
+sed -i 's/^\(    source-commit: \).*$/\1'"${upstream_release}"'/' snap/snapcraft.yaml
+
